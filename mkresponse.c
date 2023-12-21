c4c60cdad2006df8547de494ff3a110f54195b99
Vladimir Fedorov <fedorovvs@gmail.com>
2023-12-22 00:20:56 +0100

    Allocate correct amount of memory for content-length field

--- mkresponse.c
+++ mkresponse.c
@@ -22,12 +22,15 @@ int main(int argc, char **argv) {
 
 char* make_response(char *buffer, int status_code, char *status_message, char *content_type, char *content) {
     // Calculate the lengths of various parts of the HTTP response
+    // CRLF is the standard line break (https://www.w3.org/MarkUp/html-spec/html-spec_8.html#SEC8.2.1)
+    int content_length = strlen(content);
     int status_line_length = snprintf(NULL, 0, "HTTP/1.1 %d %s\r\n", status_code, status_message);
     int content_type_length = snprintf(NULL, 0, "Content-Type: %s\r\n", content_type);
-    int content_length = strlen(content);
+    int content_length_length = snprintf(NULL, 0, "Content-Length: %d\r\n", content_length);
     
     // Calculate the total length of the HTTP response
-    int total_length = status_line_length + content_type_length + content_length + 4; // 4 for \r\n\r\n and null terminator
+    // + 2 for \r\n before content
+    int total_length = status_line_length + content_type_length + content_length_length + 2 + content_length; 
     
     // Allocate memory for the complete HTTP response
     char *response = (char*)malloc(total_length);
@@ -44,7 +47,7 @@ char* make_response(char *buffer, int status_code, char *status_message, char *c
     strcat(response, content_type);
     strcat(response, "\r\n");
     // Content-Length
-    char content_length_str[30];
+    char content_length_str[content_length_length];
     sprintf(content_length_str, "Content-Length: %d\r\n", content_length);
     strcat(response, content_length_str);
     // Empty line to separate headers from the content
