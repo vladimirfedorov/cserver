71d3a13768f2ad16dfdae0e12413951727c6a509
Vladimir Fedorov <fedorovvs@gmail.com>
2024-04-09 10:25:38 +0200

    Update test suite for CI workflows

--- tests/test-cserver.c
+++ tests/test-cserver.c
@@ -14,7 +14,7 @@ int test_rendering(char *name) {
   string md_content = read_file(md_filename);
   if (md_content.value == NULL) {
     printf("failed: %s not found.\n", md_filename);
-    return -1;
+    return 1;
   }
 
   // Result test name
@@ -23,14 +23,14 @@ int test_rendering(char *name) {
   string html_content = read_file(html_filename);
   if (html_content.value == NULL) {
     printf("failed: %s not found.\n", html_filename);
-    return -1;
+    return 1;
   }
 
   cJSON *context = cJSON_CreateObject();
   string rendered_content = render_page(context, md_filename);
   if (rendered_content.value == NULL) {
     printf("failed: no result.\n");
-    return -1;
+    return 1;
   }
 
   if (strcmp(html_content.value, rendered_content.value) != 0) {
@@ -38,7 +38,7 @@ int test_rendering(char *name) {
     string_free(rendered_content);
     string_free(md_content);
     string_free(html_content);
-    return -1;
+    return 1;
   }
 
   string_free(rendered_content);
@@ -51,12 +51,14 @@ int test_rendering(char *name) {
 
 int main() {
 
-  printf("cserver tests...\n");
+  printf("Running cserver tests...\n");
+  int total = 3;
+  int failed = 0;
 
-  test_rendering("test-md-metadata");
-  test_rendering("test-md-only");
-  test_rendering("test-metadata-only");
+  failed += test_rendering("test-md-metadata");
+  failed += test_rendering("test-md-only");
+  failed += test_rendering("test-metadata-only");
 
-  printf("Done.\n");
-  return 0;
+  printf("%i tests run, %i failed.\n", total, failed);
+  return failed;
 }
